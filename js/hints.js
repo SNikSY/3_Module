54c9562c8f444fd0614bf1ff879787401e48850d
SNikSY <nsupereka92@gmail.com>
2023-03-30 23:48:17 +0300

    Final version

--- js/hints.js
+++ js/hints.js
@@ -1,4 +1,6 @@
  $(document).ready(() => {
+    
+  if(window.innerWidth > 768) {
    
      var indicator = $(".indicator_anagram");
      var indicator2 = $('.indicator_caesar');
@@ -11,7 +13,7 @@
      var indicator9 = $('.indicator_logic');
    
      indicator.click(() => {
-   
+        
        $('.hint.anagram').toggleClass("transparent");
        $('.block.alphabet').toggleClass("opacity");
        $('.block.caesar').toggleClass("opacity");
@@ -31,35 +33,11 @@
    
      });
    
-     indicator3.click(() => {
-   
-       $('.hint.lock').toggleClass("transparent");
-       $('.block.forget').toggleClass("opacity");
-       $('.block.keyboard').toggleClass("opacity");
-       $('.block.logic').toggleClass("opacity");
-       $('.block.math').toggleClass("opacity");
-   
-     });
+    
 
-     indicator4.click(() => {
+    
    
-       $('.hint.keyboard').toggleClass("transparent");
-       $('.block.tumblr').toggleClass("opacity");
-       $('.block.letter').toggleClass("opacity");
-       $('.block.maze').toggleClass("opacity");
-       $('.block.memory').toggleClass("opacity");
-   
-     });
-   
-     indicator5.click(() => {
-   
-       $('.hint.math').toggleClass("transparent");
-       $('.block.tumblr').toggleClass("opacity");
-       $('.block.letter').toggleClass("opacity");
-       $('.block.maze').toggleClass("opacity");
-       $('.block.memory').toggleClass("opacity");
-   
-     });
+     
 
      indicator6.click(() => {
    
@@ -91,15 +69,78 @@
      
      });
    
-     indicator9.click(() => {
-   
-       $('.hint.logic').toggleClass("transparent");
-       $('.block.anagram').toggleClass("opacity");
-       $('.block.alphabet').toggleClass("opacity")
-          $('.block.lock').toggleClass("opacity")
-          $('.block.forget').toggleClass("opacity")
- 
-   });
+    
+    }
+    else if(window.innerWidth < 768){
 
+      var indicator = $(".indicator_anagram");
+      var indicator2 = $('.indicator_caesar');
+      var indicator3 = $('.indicator_lock');
+      var indicator4 = $('.indicator_keyboard');
+      var indicator5 = $('.indicator_math');
+      var indicator6 = $('.indicator_alphabet');
+      var indicator7 = $('.indicator_forget');
+      var indicator8 = $('.indicator_color');
+      var indicator9 = $('.indicator_logic');
+    
+      indicator.click(() => {
+    
+        $('.hint.anagram').toggleClass("transparent");
+        $('.block.chess').toggleClass("opacity");
+        $('.block.caesar').toggleClass("opacity");
+        $('.block.color').toggleClass("opacity");
+        $('.block.lock').toggleClass("opacity");
+    
+    
+      });
+    
+      indicator2.click(() => {
+    
+        $('.hint.caesar').toggleClass("transparent");
+        $('.block.lock').toggleClass("opacity");
+        $('.block.color').toggleClass("opacity");
+        $('.block.forget').toggleClass("opacity");
+        $('.block.keyboard').toggleClass("opacity");
+    
+      });
+    
+     
+ 
+     
+    
+      
+ 
+      indicator6.click(() => {
+    
+        $('.hint.alphabet').toggleClass("transparent");
+        $('.block.chess').toggleClass("opacity");
+        $('.block.caesar').toggleClass("opacity");
+        $('.block.color').toggleClass("opacity");
+        $('.block.lock').toggleClass("opacity");
+    
+      });
+    
+      indicator7.click(() => {
+    
+        $('.hint.chess').toggleClass("transparent");
+        $('.block.letter').toggleClass("opacity");
+        $('.block.tumblr').toggleClass("opacity");
+        $('.block.logic').toggleClass("opacity");
+        $('.block.light').toggleClass("opacity");  
+    
+      });
+    
+      indicator8.click(() => {
+    
+        $('.hint.color').toggleClass("transparent");
+        $('.block.keyboard').toggleClass("opacity");
+        $('.block.tumblr').toggleClass("opacity");
+        $('.block.forget').toggleClass("opacity");
+        $('.block.letter').toggleClass("opacity");
+      
+      });
+
+
+    }
  });
    
--- js/main.js
+++ js/main.js
@@ -1,45 +1,63 @@
 $(document).ready(function() {
-     var correct_words = ["secure", "recuse"];
-
-     // When a button with class "char-btn" is clicked
-     $(".button_anagram").on("click", function() {
-          // Get the data-char attribute of the button clicked
-          var char = $(this).data("char");
-
-           // Append the character to the output div
-          $(".display_anagram_bottom").append(char);
-      });
-      // Add a click event listener to the check button
-     $(".check_anagram").on("click", function() {
-          // Get the current text of the output div
-          var output_text = $(".display_anagram_bottom").text().trim().toLowerCase();
-          output_text = output_text.replace(/\s/g, '');
-          // Compare the output text with the correct words
-          if (correct_words.indexOf(output_text) !== -1) {
-               $(".indicator_anagram").addClass("correct")
-          }
-     });
-
-
-      
-      $(".delete_anagram").on("click", function() {
-          $(".display_anagram_bottom").text("");
-      });
-
- });
+   
+   $('.start').click(() => {
+    
+    $('.start-screen').delay(200).fadeOut();
+    $('body').css('overflow', 'auto');
+  });
+  
+  
+  $('body').css('overflow', 'hidden');
+  
 
 
 
+  const correct_words = ["secure", "recuse"];
+
+  
+  $(".button_anagram").on("click", (event) => {
+    
+    const char = $(event.currentTarget).data("char");
+
+    
+    $(".display_anagram_bottom").append(char);
+    $(event.currentTarget).addClass("clicked");
+    setTimeout(() => $(event.currentTarget).removeClass("clicked"), 200);
+  });
+  
+
+  $(".check_anagram").on("click", (event) => {
+    $(event.currentTarget).addClass("clicked");
+    setTimeout(() => $(event.currentTarget).removeClass("clicked"), 200);
+    
+    let output_text = $(".display_anagram_bottom").text().trim().toLowerCase();
+    output_text = output_text.replace(/\s/g, '');
+    
+    if (correct_words.indexOf(output_text) !== -1) {
+      $(".indicator_anagram").addClass("correct")
+      $("#three").css("background-color", "greenyellow")
+    }
+  });
+  
+  $(".delete_anagram").on("click", (event) => {
+    $(".display_anagram_bottom").text("");
+    $(event.currentTarget).addClass("clicked");
+    setTimeout(() => $(event.currentTarget).removeClass("clicked"), 200);
+  });
 
 
- const correctOrder = ['H', 'D', 'U'];
+
+
+  const correctOrder = ['H', 'D', 'U'];
   let progress = 0;
   let numIncorrect = 0;
-
+// Почему-то стрелочная тут всё ломает
   $('.button_alphabet').on('click', function() {
     const $button = $(this);
     const letter = $button.find('p').text();
     const $indicator = $button.find('.button_alphabet_indicator');
+    $(this).addClass("clicked");
+    setTimeout(() => $(this).removeClass("clicked"),200);
 
     if (letter === correctOrder[progress]) {
       $button.addClass('selected');
@@ -48,6 +66,7 @@ $(document).ready(function() {
 
       if (progress === correctOrder.length) {
         $('.indicator_alphabet').addClass('lit-green');
+        $("#four").css("background-color", "greenyellow")
       }
     } else {
       $button.addClass('selected');
@@ -69,10 +88,7 @@ $(document).ready(function() {
   });
 
 
-
-
-  $(document).ready(function() {
-     var expectedOrder = "YJKZE";
+  var expectedOrder = "YJKZE";
      var currentIndex = 0;
    
      $(".button_caesar").click(function() {
@@ -85,253 +101,287 @@ $(document).ready(function() {
          currentIndex++;
          if (currentIndex == expectedOrder.length) {
            $(".indicator_caesar").css("background-color", "greenyellow");
+           $("#five").css("background-color", "greenyellow")
          }
        } else {
          currentIndex = 0;
          $(".indicator_caesar").css("background-color", "red");
        }
      });
-   });
+    
+    
+     const colorNames = ["RED", "BLUE", "GREEN", "YELLOW", "PURPLE", "ORANGE"];
+     let indicatorCounter = 0;
+     let colorIndex, textColorIndex;
+     let buttonsDisabled = false;
+     
+     const randomColor = () => {
+       colorIndex = Math.floor(Math.random() * colorNames.length);
+       textColorIndex = Math.floor(Math.random() * colorNames.length);
+       $(".display_color_right").text(colorNames[colorIndex]).css("color", colorNames[textColorIndex]);
+       if (buttonsDisabled) {
+         $("#yes-button, #no-button").prop("disabled", true);
+       } else {
+         $("#yes-button, #no-button").prop("disabled", false);
+       }
+     };
+     
+     $(".button_color").on("click", (event) => {
+       $(event.currentTarget).addClass("clicked");
+       setTimeout(() => $(event.currentTarget).removeClass("clicked"), 200);
+     });
+     
+     $("#yes-button, #no-button").on("click", (event) => {
+       if (buttonsDisabled) {
+         return;
+       }
+       if (
+         ($(event.target).attr("id") === "yes-button" && colorIndex === textColorIndex) ||
+         ($(event.target).attr("id") === "no-button" && colorIndex !== textColorIndex)
+       ) {
+         indicatorCounter++;
+         if (indicatorCounter === 5) {
+           $(".indicator_color").css("background-color", "greenyellow");
+           $("#six").css("background-color", "greenyellow");
+           indicatorCounter = 0;
+           buttonsDisabled = true;
+           $("#yes-button, #no-button").prop("disabled", true);
+         }
+       } else {
+         indicatorCounter = 0;
+         $(".indicator_color").css("background-color", "#ccc");
+       }
+       randomColor();
+     });
+     
+     randomColor();
+     
 
 
-
-
-
-
-
-   const colorNames = ["RED", "BLUE", "GREEN", "YELLOW", "PURPLE", "ORANGE"];
-		let indicatorCounter = 0;
-		let colorIndex, textColorIndex;
-		let buttonsDisabled = false;
-
-		const randomColor = () => {
-			colorIndex = Math.floor(Math.random() * colorNames.length);
-			textColorIndex = Math.floor(Math.random() * colorNames.length);
-			$(".display_color_right").text(colorNames[colorIndex]).css("color", colorNames[textColorIndex]);
-			if (buttonsDisabled) {
-				$("#yes-button, #no-button").prop("disabled", true);
-			} else {
-				$("#yes-button, #no-button").prop("disabled", false);
-			}
-		};
-          $(".button_color").on("click", (event) => {
-               $(event.currentTarget).addClass("clicked");
-               setTimeout(() => $(event.currentTarget).removeClass("clicked"),200);
-          })
-		$("#yes-button, #no-button").on("click", (event) => {
-              
-			if (buttonsDisabled) {
-				return;
-			}
-			if ($(event.target).attr("id") === "yes-button" && colorIndex === textColorIndex
-				|| $(event.target).attr("id") === "no-button" && colorIndex !== textColorIndex) {
-				indicatorCounter++;
-				if (indicatorCounter === 5) {
-					$(".indicator_color").css("background-color", "greenyellow");
-					indicatorCounter = 0;
-					buttonsDisabled = true;
-					$("#yes-button, #no-button").prop("disabled", true);
-				}
-			} else {
-                    indicatorCounter = 0;
-			$(".indicator_color").css("background-color", "#ccc");
-		}
-		randomColor();
-	});
-
-	randomColor();
-
-
-  $(document).ready(function() {
-    var currentDisplay = Math.floor(Math.random() * 9); // initialize random starting display value
+  var currentDisplay = Math.floor(Math.random() * 9); 
     var correctAnswers = [
-      Math.max(currentDisplay - 3, 1), // first answer is 3 digits smaller than current display, but not less than 0
-      Math.min(currentDisplay + 1, 9), // second answer is 1 digit bigger than current display
-      Math.min(currentDisplay + 7, 9) // third answer is 7 digits bigger than current display
+      Math.max(currentDisplay - 3, 1), 
+      Math.min(currentDisplay + 1, 9), 
+      Math.min(currentDisplay + 7, 9) 
     ];
     var currentShift = 0;
     var enteredDigits = '';
     $('.display_forget').text(currentDisplay);
     $('.button_forget').click(function() {
+      $(this).addClass("clicked");
+      setTimeout(() => $(this).removeClass("clicked"),200);
       var clickedButton = $(this).text();
       $('.display_forget_right').text($('.display_forget_right').text() + clickedButton);
       enteredDigits += clickedButton;
       if (enteredDigits.length == 3) {
         if (enteredDigits == correctAnswers.join('')) {
           $('.indicator_forget').css('background-color', '#adff2f')
+          $("#seven").css("background-color", "greenyellow")
           currentShift = 0;
-        } else {
-          alert('Sorry, incorrect answer. Please try again.');
         }
         enteredDigits = '';
         currentDisplay = Math.floor(Math.random() * 10);
         correctAnswers = [
-          Math.max(currentDisplay - 3, 0),
-          Math.min(currentDisplay + 1, 9), // second answer is 1 digit bigger than current display
+          Math.max(currentDisplay - 3, 1),
+          Math.min(currentDisplay + 1, 9), 
           Math.min(currentDisplay + 7, 9)
         ];
         $('.display_forget').text(currentDisplay);
         $('.display_forget_right').text('');
       }
     });
-  });
-  
 
 
-  const correctOrderTwo = ['3', '1', '2','4'];
-  let progressTwo = 0;
-  let numIncorrectTwo = 0;
-
-  $('.button_keyboard').on('click', function() {
-    const $button = $(this);
-    const letter = $button.find('p').text();
-    const $indicator = $button.find('.button_keyboard_indicator');
-
-    if (letter === correctOrderTwo[progress]) {
-      $button.addClass('selected');
-      $indicator.css('background-color', '#adff2f')
-      progress++;
-
-      if (progress === correctOrderTwo.length) {
-        $('.indicator_keyboard').css('background-color', '#adff2f')
-      }
-    } else {
-      $button.addClass('selected');
-      $indicator.css('background-color', 'ff0000')
-      progressTwo = 0;
-      numIncorrectTwo++;
-
-      if (numIncorrectTwo === $('.button_keyboard').length) {
-        $('.indicator_keyboard, .button_keyboard_indicator').css('background-color', '555555')
-        $('.button_alphabet').removeClass('selected');
-        numIncorrectTwo = 0;
-      } else {
-        setTimeout(function() {
-          $button.removeClass('selected');
-          $indicator.css('background-color', '555555')
-        }, 500);
-      }
-    }
-  });
-
-
-
-
-
-  $(document).ready(function() {
-  var timer;
-  $(".button_light:first").one("click", function() {
-    var count = parseInt($(".display_light_right").text());
-    timer = setInterval(function() {
-      count--;
-      $(".display_light_right").text(count);
-      if (count == 0) {
-        clearInterval(timer);
-      }
-    }, 1000);
-  });
-
-  $(".button_light").click(function() {
-    $(this).css("background-color", "greenyellow");
-    if ($(".button_light:not(:has(style))").length == 0) {
-      clearInterval(timer);
-    } else if (!timer) {
+    var timer;
+    $(".button_light:first").one("click", function() {
+      var count = parseInt($(".display_light_right").text());
       timer = setInterval(function() {
-        var count = parseInt($(".display_light_right").text());
         count--;
         $(".display_light_right").text(count);
         if (count == 0) {
           clearInterval(timer);
         }
       }, 1000);
-    }
-  });
-});
+    });
+  
+    $(".button_light").click(function() {
+      $(this).css("background-color", "greenyellow");
+      if ($(".button_light:not(:has(style))").length == 0) {
+        clearInterval(timer);
+      } else if (!timer) {
+        timer = setInterval(function() {
+          var count = parseInt($(".display_light_right").text());
+          $(".display_light_right").text(count);
+          if (count == 0) {
+            clearInterval(timer);
+            $("#eight").css("background-color", "greenyellow")
+          }
+        }, 1000);
+      }
+    });
+
+
+
+
+
+
+
+    
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   
+  function generateProblem() {
+    
+    var num1 = numbers[Math.floor(Math.random() * numbers.length)];
+    var num2 = numbers[Math.floor(Math.random() * numbers.length)];
 
+    
+    if (num1 < num2) {
+      var temp = num1;
+      num1 = num2;
+      num2 = temp;
+    }
 
+    
+    var problem = num1 + " - " + num2;
 
-
-
-
-
-// Define an array of numbers to use in the problems
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// Define a function to generate a random problem
-function generateProblem() {
-  // Pick two random numbers from the array
-  var num1 = numbers[Math.floor(Math.random() * numbers.length)];
-  var num2 = numbers[Math.floor(Math.random() * numbers.length)];
-
-  // Make sure num1 is greater than num2
-  if (num1 < num2) {
-    var temp = num1;
-    num1 = num2;
-    num2 = temp;
+    
+    return problem;
   }
 
-  // Build the problem string
-  var problem = num1 + " - " + num2;
+  
+  function updateProblem() {
+    var problem = generateProblem();
 
-  // Return the problem string
-  return problem;
-}
-
-// Define a function to update the problem display
-function updateProblem() {
-  // Generate a new problem
-  var problem = generateProblem();
-
-  // Update the display
-  $(".display_math_left").text(problem);
-}
-
-// Call the updateProblem function to generate the first problem
-updateProblem();
-
-// Add a click handler to the equals button to check the answer
-$(".buttons_math .button_math:contains('=')").click(function() {
-  // Get the user's answer from the display
-  var userAnswer = $(".display_math_center").text();
-
-  // Calculate the correct answer
-  var problemParts = $(".display_math_left").text().split(" - ");
-  var correctAnswer = problemParts[0] - problemParts[1];
-
-  // Check the user's answer
-  if (userAnswer == correctAnswer) {
-    $(".indicator_math").css("background-color", "greenyellow");
-    $(".display_math_center").text(" ")
-  } else {
-    $(".indicator_math").css("background-color", "red");
-    $(".display_math_center").text(" ")
+    
+    $(".display_math_left").text(problem);
   }
 
-  // Update the problem display with a new problem
+ 
   updateProblem();
-});
 
-// Add a click handler to the number buttons to update the display
-$(".buttons_math .button_math:not(:contains('=')):not(:contains('-'))").click(function() {
-  // Get the clicked button's text
-  var buttonText = $(this).text();
+  
+  $(".buttons_math .button_math:contains('=')").click(function() {
+   
+    var userAnswer = $(".display_math_center").text();
 
-  // Get the current display text
-  var displayText = $(".display_math_center").text();
+   
+    var problemParts = $(".display_math_left").text().split(" - ");
+    var correctAnswer = problemParts[0] - problemParts[1];
 
-  // Update the display with the clicked button's text
-  $(".display_math_center").text(displayText + buttonText);
-});
+   
+    if (userAnswer == correctAnswer) {
+      $(".indicator_math").css("background-color", "greenyellow");
+      $("#nine").css("background-color", "greenyellow")
+      $(".display_math_center").text(" ")
+    } else {
+      $(".indicator_math").css("background-color", "red");
+      $(".display_math_center").text(" ")
+    }
 
-// Add a click handler to the minus button to update the display
-$(".buttons_math .button_math:contains('-')").click(function() {
-  // Get the current display text
-  var displayText = $(".display_math_center").text();
+   
+    updateProblem();
+  });
 
-  // If the display is empty, add a minus sign
-  if (displayText == "") {
-    $(".display_math_center").text("-");
+  
+  $(".buttons_math .button_math:not(:contains('=')):not(:contains('-'))").click(function() {
+   
+    var buttonText = $(this).text();
+
+   
+    var displayText = $(".display_math_center").text();
+
+   
+    $(".display_math_center").text(displayText + buttonText);
+  });
+
+  
+  $(".buttons_math .button_math:contains('-')").click(function() {
+    
+    var displayText = $(".display_math_center").text();
+
+  
+    if (displayText == "") {
+      $(".display_math_center").text("-");
+    }
+  });
+
+
+
+
+
+
+
+
+var numDisplayed;
+  var buttonToPress;
+  var numCorrect = 0;
+
+  function generateNumber() {
+    numDisplayed = Math.floor(Math.random() * 4) + 1;
+    $('.display_memory').text(numDisplayed);
+  
+  
+    if (numDisplayed === 1) {
+      buttonToPress = $('.buttons_memory').find('.button_memory:nth-child(2)');
+    } else if (numDisplayed === 2) {
+      buttonToPress = $('.buttons_memory').find('.button_memory:nth-child(1)');
+    } else if (numDisplayed === 3) {
+      buttonToPress = $('.buttons_memory').find('.button_memory:nth-child(3)');
+    } else if (numDisplayed === 4) {
+      buttonToPress = $('.buttons_memory').find('.button_memory:nth-child(1)');
+    }
   }
+  
+  function updateIndicators() {
+    var indicatorToLight = $('.indicator_memory_right:not(.lit)').first();
+    if (indicatorToLight.length > 0) {
+      indicatorToLight.addClass('lit');
+     
+    }
+  }
+  
+  function resetGame() {
+    $('.indicator_memory').addClass('lit');
+    numCorrect = 0;
+    $("#ten").css("background-color", "greenyellow")
+    generateNumber();
+  }
+
+  
+  generateNumber();
+  
+  
+  $('.button_memory').on('click', function() {
+    if ($(this).index() === buttonToPress.index()) {
+      numCorrect++;
+      updateIndicators();
+      if (numCorrect === 5) {
+        
+        resetGame();
+      } else {
+        
+        generateNumber();
+      }
+    }
+  });
+
+
+  function handleButtonClick() {
+    $(this).addClass("clicked");
+    setTimeout(() => $(this).removeClass("clicked"), 200);
+  }
+  
+  $(".button_chess").click(handleButtonClick);
+  $(".button_lock").click(handleButtonClick);
+  $(".button_lock_right").click(handleButtonClick);
+  $(".start").click(handleButtonClick);
+  $(".button_keyboard").click(handleButtonClick);
+  $(".button_letter").click(handleButtonClick);
+  $(".button_light").click(handleButtonClick);
+  $(".button_logic").click(handleButtonClick);
+  $(".button_math").click(handleButtonClick);
+  $(".button_memory").click(handleButtonClick);
+  
 });
+  
